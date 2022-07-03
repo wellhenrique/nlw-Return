6b9acda55d69835923a1552ac5c23ba8b2444e42
wellhenrique <welissonh80@gmail.com>
2022-07-03 19:54:37 -0300

    feat: create feedbacks

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
 
 const app = express();
+app.use(express.json());
 
-app.post("/feedback", (req, res) => {
-  const { comment, image } = req.body;
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      screenshot,
+      comment,
+    },
+  });
+
+  res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => console.log("HTTP server running!"));
