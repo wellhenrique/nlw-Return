fc57321f52ba2d582e080e6707fd4d4740a28801
wellhenrique <welissonh80@gmail.com>
2022-07-04 21:48:26 -0300

    feat: sendEmail nodemailer

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,7 +3,17 @@ import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
+
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "c89893cf5fe663",
+    pass: "e508e2e097a731",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       screenshot,
       comment,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Welisson Henrique <welissonh80@gmail.com>",
+    subject: "Novo Feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentario: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   res.status(201).json({ data: feedback });
