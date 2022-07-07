2413afe3a5bbc74ca665e9754dc078cd4c127bd0
wellhenrique <welissonh80@gmail.com>
2022-07-07 15:01:59 -0300

    feat: add Wiget component

--- packages/expo/App.tsx
+++ packages/expo/App.tsx
@@ -1,23 +1,37 @@
+import React from "react";
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
-import { Widget } from "./src/components/Widget";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+
+import Widget from "./src/components/Widget";
+
 import { theme } from "./src/theme";
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: theme.colors.background
-    }}>
-      
-      <Widget />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
 
-      <StatusBar 
-      style="light"
-      backgroundColor="transparent"
-      translucent 
-      />
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+
+      <Widget />
     </View>
   );
 }
-
--- packages/expo/src/components/Widget/index.tsx
+++ packages/expo/src/components/Widget/index.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from "phosphor-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
-import { styles } from './styles';
+import { theme } from "../../theme";
+import { styles } from "./styles";
+import { Options } from "../Options";
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpenWidget() {
+    bottomSheetRef.current?.expand();
+  }
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpenWidget}>
+        <ChatTeardropDots size={24} color="white" weight="bold" />
+      </TouchableOpacity>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
+    </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
