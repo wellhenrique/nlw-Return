1b3ebe35467a032aa37c4b741f612ecc02e98152
wellhenrique <welissonh80@gmail.com>
2022-07-09 23:24:50 -0300

    chore: add styles Options feedback

--- packages/expo/src/components/Options/styles.ts
+++ packages/expo/src/components/Options/styles.ts
@@ -1,7 +1,25 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
     alignContent: "center",
+    justifyContent: "center",
+    width: 104,
+    height: 112,
+    padding: 8,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: theme.colors.surface_secondary,
+  },
+  image: {
+    width: 40,
+    height: 40,
+  },
+  title: {
+    fontSize: 14,
+    marginTop: 8,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_primary,
   },
 });
